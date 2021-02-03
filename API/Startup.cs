583009ccc3f56ba81883cd931c105ee38f5b23b2
junhanliu <junhanliu.dev@gmail.com>
2021-02-03 13:30:45 +1100

    completed web-app

--- API/Startup.cs
+++ API/Startup.cs
@@ -39,10 +39,10 @@ namespace API
       services.AddMediatR(typeof(List.Handler).Assembly);
 
       services.AddControllers();
-      // services.AddSwaggerGen(c =>
-      // {
-      //   c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-      // });
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +52,12 @@ namespace API
       {
         app.UseDeveloperExceptionPage();
       }
+
+      app.UseSwagger();
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "utu API v1");
+      });
 
       app.UseCors("CorsPolicy");
       app.UseHttpsRedirection();
